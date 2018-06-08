bb8afde2f5ac665ec863cdf852ee1f9d578fd900
Max Pereshybkin <max.prs@gmail.com>
2018-06-08 13:25:30 +0300

    Code update

--- map1.py
+++ map1.py
@@ -1,3 +1,27 @@
 import folium
-map = folium.Map(location=[38.58,-90.09],start_zoom=6)
+import pandas
+
+
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
+map = folium.Map(location=[38.58,-90.09],zoom_start=6, tiles="Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My Map")
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el) + " m",color="grey",fill_color=color_producer(el), fill=True, fill_opacity=0.7))
+map.add_child(fg)
+
 map.save("Map1.html")
